8b0749ddcde5a6e599b18991c72fa3b89c4a5475
jdw3314 <jdw3314@gmail.com>
2022-12-11 22:29:16 +0900

    1
    1

---
+++ 3D-TeamProject/Assets/Scripts/HitPlatForm.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HitPlatForm : MonoBehaviour
+{
+    protected int HitDamage = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
