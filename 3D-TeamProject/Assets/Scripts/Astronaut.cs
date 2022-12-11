ba1d525bc34cbb6836db348551b0db4ce0f68bb0
skcjswo <iwannagenius@naver.com>
2022-12-12 01:40:53 +0900

    f
    f

--- 3D-TeamProject/Assets/Scripts/Astronaut.cs
+++ 3D-TeamProject/Assets/Scripts/Astronaut.cs
@@ -2,89 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Anim
-{
-	public AnimationClip idle;
-	public AnimationClip walk;
-	public AnimationClip Jump_loop;
-}
-
+//안녕
 public class Astronaut : MonoBehaviour
 {
-	[SerializeField]
+    public float speed;
+    static public int PlayerHp = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello!!");
+    }
 
-	private bool isRun = false;
+    void Update()
+    {
+        // w ->앞
+        if (Input.GetKey(KeyCode.D))
+        {
 
-	private Rigidbody Rigid;
+            transform.Translate(Vector2.right * speed);
 
-	public Animation anim;
-	public AnimationClip clip;
+        }
 
-	public float speed = 10f;
-	static public int PlayerHp = 500;
-		
-		// Start is called before the first frame update
-	void Start()
-	{
-		Debug.Log("Hello!!");
-		anim = GetComponentInChildren<Animation>();
-		Rigid = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+        {
 
-		
-	}
+            transform.Translate(-Vector2.right * speed);
 
-	void Update()
-	{
-		 void run()
-		 {
-			isRun = true;
- 
-		 }
-		if (Input.GetMouseButtonDown(1))
-		{
-			run();
-			Debug.Log("스피드");
-			speed = speed * 1.6f;
-			
-		}
-		// w ->앞
-		if (Input.GetKey(KeyCode.D))
-		{
+        }
 
-			transform.Translate(Vector2.right * speed);
+        if (Input.GetKey(KeyCode.W))
+        {
 
-		}
+            transform.Translate(Vector2.up * speed);
 
-		if (Input.GetKey(KeyCode.A))
-		{
+        }
 
-			transform.Translate(-Vector2.right * speed);
+        if (Input.GetKey(KeyCode.S))
+        {
 
-		}
+            transform.Translate(-Vector2.up * speed);
+        }
+    }
 
-		if (Input.GetKey(KeyCode.W))
-		{
+    static public void PlayerDie()
+    {
+        Debug.Log("Player Die!!");
+    }
 
-			transform.Translate(Vector2.up * speed);
 
-		}
-
-		if (Input.GetKey(KeyCode.S))
-		{
-
-			transform.Translate(-Vector2.up * speed);
-		}
-
-		
-	}
-
-	static public void PlayerDie()
-	{
-		Debug.Log("Player Die!!");
-	}
-
-	
 }
-
-
